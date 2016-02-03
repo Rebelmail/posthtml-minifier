13d8948290562bfdac789a8c40f83728a4b57f05
Devin Torres <devin@devintorr.es>
2016-02-03 12:27:34 -0600

    Initial commit

---
+++ test/plugin.js
@@ -0,0 +1,23 @@
+var posthtml = require('posthtml');
+var expect = require('chai').expect;
+
+var minifier = require('..');
+
+describe('posthtml-minifier', function() {
+  it('minifies HTML', function(done) {
+    var value = '<html><body><!-- a comment --></body></html>';
+    var expected = '<html><body></body></html>';
+    var options = { removeComments: true };
+
+    posthtml()
+      .use(minifier(options))
+      .process(value)
+      .then(function(result) {
+        expect(result).to.exist;
+        expect(result.html).to.equal(expected);
+        done();
+      }).catch(function(err) {
+        done(err);
+      });
+  });
+});
